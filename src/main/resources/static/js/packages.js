45915032c92c49762d216e14ac63ea1b390e8727
Jordan Abe <j.abe@costamar.com>
2020-02-13 17:47:49 -0500

    Se agrega lógica para agregar filas en una table

--- src/main/resources/static/js/packages.js
+++ src/main/resources/static/js/packages.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
 	
-//	$('button[type="submit"]').on('click', function(e) {
-//		e.preventDefault();
-//		   $.post($("#form").attr('action'), function(request, response) {console.log(request)});
-//	});
-	
-	
 	$('#addOptionalServiceClass').on('click', function() {
-        if ($('#optionalServiceClass').val().length == 1) {
+//        if ($('#optionalServiceClass').val().length == 1) {
             var optionalServiceClass = $('<div>',{
                 class: 'btn btn-outline-success btn-sm m-1',
                 style: 'min-width: 64px;'
@@ -25,52 +19,52 @@ $(document).ready(function () {
                 value: $('#optionalServiceClass').val()
             }))
             $('#optionalServicesClass').append(optionalServiceClass)
-        }
+//        }
     });
     $(document).on('click', '.removeOptionalServiceClass', function() {
         $(this).closest('.btn').remove()
     });
     
-    
     $('#idAddSegment').on('click', function() {
-    	console.log('add');
-//    	var tr = $('<tr>',{
-//            class: 'btn btn-outline-success btn-sm m-1',
-//            style: 'min-width: 64px;'
-//        })
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idAirline').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idFlightNumber').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idAirportOrigin').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idDepartureDate').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idDepartureTime').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idAirportDestination').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idArrivalDate').val()
-//        }))
-//        tr.append($('<td>',{
-//            class: 'pr-1',
-//            text: $('#idArrivalTime').val()
-//        }))
-//    	$('#idTbody').append(tr)
+        var newIndex = $('#idTbody').prop('childElementCount'); + 1;
+        var tr = $('<tr>',{
+            class: '',
+            style: ''
+        })
+        tr.append(buildCol($('#idAirline').val(), newIndex, 'airline.code'));
+        tr.append(buildCol($('#idFlightNumber').val(), newIndex, 'flightNumber'));
+        tr.append(buildCol($('#idAirportOrigin').val(), newIndex, 'airportOrigin.codeIATA'));
+        tr.append(buildCol($('#idDepartureDate').val(), newIndex, 'departureDate'));
+        tr.append(buildCol($('#idDepartureTime').val(), newIndex, 'departureTime'));
+        tr.append(buildCol($('#idAirportDestination').val(), newIndex, 'airportDestination.codeIATA'));
+        tr.append(buildCol($('#idArrivalDate').val(), newIndex, 'arrivalDate'));
+        tr.append(buildCol($('#idArrivalTime').val(), newIndex, 'arrivalTime'));
+        tr.append(buildCol($('#idIndicator').val(), newIndex, 'Indicator'));
+    	$('#idTbody').append(tr);
     });
 
 });
+
+function buildCol(value, index, sufijo){
+    var td = $('<td>',{
+        class: '',
+        style: ''
+    })
+    var label = $('<label>',{
+        class: '',
+        style: '',
+        text: '' + value
+    })
+    var input = $('<input>',{
+        class: '',
+        style: '',
+        type: 'hidden',
+        id: 'segmentsUtil' + index + '.' + sufijo,
+        name: 'segmentsUtil[' + index + '].' + sufijo,
+        value: '' + value
+    })
+    td.append(label)
+    td.append(input)
+    return td;
+}
+
