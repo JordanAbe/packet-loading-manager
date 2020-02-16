4845a61479033674f1aec563a2c61a6261f23ede
Jordan Abe <j.abe@costamar.com>
2020-02-16 18:26:07 -0500

    se agrega fontawasome

--- src/main/java/tech/zdev/packages/loading/packetloadingmanager/config/MvcConfig.java
+++ src/main/java/tech/zdev/packages/loading/packetloadingmanager/config/MvcConfig.java
@@ -52,6 +52,7 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
 		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/webfonts/**").addResourceLocations("classpath:/static/webfonts/");
 	}
-
+	
 }
